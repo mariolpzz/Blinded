1130e5ae55c9f812fcc5b05cffbe29072cee736b
Mario López <lmario846@gmail.com>
2022-11-01 17:49:18 +0100

    Prueba básica de animaciones
    El jugador simplemente puede moverse hacia delante y tiene una animación de "Run"

---
+++ Blinded/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Animator playerAnim;
+    public float speed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var velocity = Vector3.forward * Input.GetAxis("Vertical") * speed;
+        transform.Translate(velocity * Time.deltaTime);
+        playerAnim.SetFloat("Speed_f", velocity.magnitude);
+    }
+}
