2bb8093eb67803b679224703471f8fefec33bc43
Mario López <lmario846@gmail.com>
2022-11-06 14:33:53 +0100

    Added lantern
    Basic functioning of a lantern

---
+++ Blinded/Assets/Scripts/PointLantern.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointLantern : MonoBehaviour
+{
+
+    Vector3 lookPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            lookPos = hit.point;
+        }
+
+        Vector3 lookDir = lookPos - transform.position;
+        lookDir.y = 0;
+
+        transform.LookAt(transform.position + lookDir, Vector3.up);
+    }
+}
