fc8e10212660941b841a99536389a89c184dd10f
Mario López <lmario846@gmail.com>
2022-11-08 12:57:04 +0100

    Advanced with wall transparency when obstructing
    It works with single material objects

--- Blinded/Assets/Scripts/SeeThroughWallsCamera.cs
+++ Blinded/Assets/Scripts/SeeThroughWallsCamera.cs
@@ -7,8 +7,8 @@ public class SeeThroughWallsCamera : MonoBehaviour
 {
 
     public GameObject player;
-    public Material transparent;
-    public Material baseMaterial;
+    public bool obstructed = false;
+    private Material baseMaterial;
 
     // Start is called before the first frame update
     void Start()
@@ -19,20 +19,29 @@ public class SeeThroughWallsCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Creo un RaycastHit que me devolvera info de donde apunta la camara
         RaycastHit hit = new RaycastHit();
 
+        //Esto es para ver en el inspector el rayo que simula a donde esta apuntando la camara
         Debug.DrawRay(transform.position, player.transform.position - transform.position + new Vector3(0, 1, 0), Color.red);
 
         if (Physics.Raycast(transform.position, player.transform.position - transform.position + new Vector3(0, 1, 0), out hit, Mathf.Infinity))
         {
             Debug.Log(hit.transform.tag);
+            //Si no esta "viendo" al Player, bajo el componente "Alpha" del material que esta obstruyendo
             if (hit.collider.gameObject.tag != "Player")
             {
-                baseMaterial = hit.transform.GameObject().GetComponent<MeshRenderer>().material;
-                hit.transform.GameObject().GetComponent<MeshRenderer>().material = transparent;
-            } else
+                baseMaterial = hit.transform.GameObject().GetComponent<Renderer>().material;
+                Color colorAux = baseMaterial.color;
+                colorAux.a = 0.4f;
+                baseMaterial.color = colorAux;
+                obstructed = true;
+            } else if (obstructed)
             {
-                hit.transform.GameObject().GetComponent<MeshRenderer>().material = baseMaterial;
+                Color colorAux = baseMaterial.color;
+                colorAux.a = 1;
+                baseMaterial.color = colorAux;
+                obstructed = false;
             }
         }
     }
