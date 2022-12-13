61d2d37e443bf9e9f1e469deed70d7fdc2441561
MateuszYSZ <101577908+MateuszYSZ@users.noreply.github.com>
2022-12-13 22:51:25 +0100

    Multiplayer 1.0

---
+++ Blinded/Assets/Scripts/NetworkManagerUI.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetworkManagerUI : MonoBehaviour
+{
+
+    [SerializeField] private Button serverButton;
+    [SerializeField] private Button hostButton;
+    [SerializeField] private Button clientButton;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        serverButton.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartServer();
+        });
+
+        hostButton.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartHost();
+        });
+
+        clientButton.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartClient();
+        });
+    }
+}
